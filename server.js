b8298b93f669856946b38f9949cdb3a994ac491e
niteenautade <niteenautade@gmail.com>
2017-07-02 22:47:13 +0530

    local login setup

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
 var express  = require('express');
 var app      = express();
-var path = require('path');
 var port     = process.env.PORT || 4200;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var path     = require('path');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
 console.log('Buss',path.join(__dirname, '/src/app/dist/index.html'));
 app.use('/app', express.static(path.join(__dirname, '/src/app/dist')));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/src/app/dist/index.html'))
-})
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
